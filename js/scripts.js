f6f3e12a7be2aa95297da9c82d1e95d650ea7d6d
Andre Baptista <122994174+andrelbaptista@users.noreply.github.com>
2024-08-22 13:36:30 -0300

    FILED VALIDATION AND COPY FUNCTION

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
-"use stricti";
+// usando strict mode
+"use strict";
 
 //
 //
@@ -13,6 +14,30 @@ const passwordBtn = document.querySelector("#btn-password-gen");
 
 // seleciona a div de senha gerada
 const passwordElement = document.querySelector("#generated-password");
+
+// seleciona botão que permite alterar formatação da senha
+const btnPassGen = document.querySelector("#btn-generate");
+
+// seleciona container que esta hide
+const showUp = document.querySelector("#generate-options");
+
+// seleciona quantos carecteres a senha ira ter
+const lengthInput = document.querySelector("#length");
+
+// seleciona se a senha terá letras
+const letterInput = document.querySelector("#letter");
+
+// seleciona se a senha terá números
+const numberInput = document.querySelector("#number");
+
+// seleciona se a senha terá símbolos
+const symbolInput = document.querySelector("#symbol");
+
+// seleciona botão copy
+const copyBtn = document.querySelector("#copy-password");
+
+// limite nde 30 caracteres
+const limite30 = document.querySelector("#warning");
 
 //
 //
@@ -55,43 +80,70 @@ const getNumberAnotherWay = () => {
   return Math.floor(Math.random() * 10).toString();
 };
 
-
 // cria a senha, executando as funções de forma aleatória e selecionando somente 10 caracteres
-// 
-const genPassword = (getLetterLowercase,
-getLetterUpercase,
-getSymbol,
-getNumber,
-getNumberAnotherWay) => {
-    // zera a senha, define o tamanho e as funçoes a serem utilizadas
-    let password = "";
-    const passwordLength = 10;
-    const funcGenerator = [getLetterLowercase,
-        getLetterUpercase,
-        getSymbol,
-        getNumber];
-        // pra ficar 4 e dar problema de tamanho da senha 12 ao invés de 10
-        // getNumberAnotherWay];
+//
+const genPassword = (
+  getLetterLowercase,
+  getLetterUpercase,
+  getSymbol,
+  getNumber
+) => {
+  // zera a senha, define o tamanho e as funçoes a serem utilizadas
+  let password = "";
 
-        // começa a gerar a senha alternando entre as funções que são 5 e limitadas a 10 caracteres
-        for(i=0; i < passwordLength; i = i + funcGenerator.length) {
-            // percorre o array, executando as funções de forma aleatória
-            funcGenerator.forEach(() => {
-                const randomValue = funcGenerator[Math.floor(Math.random() * funcGenerator.length)]();
-                password += randomValue;
-            })
-        }
+  // recebe o tamanho de senha escolhido pelo usuário. O símbolo + força a entender o length como número e não string
+  let passwordLength = +lengthInput.value;
 
-        // retira os dois carecteres adicionais pra ficar em 10
-        password = password.slice(0, passwordLength);
-        
-        // tiro o display none da div pra mostrar a senha e coloca no h4 a senha gerada
-        passwordElement.style.display = "block";
-        passwordElement.querySelector("h4").innerText = password;
+  // inicializa o array de funçoes vazio
+  const funcGenerator = [];
 
+  // adiciona ao array de funções os tipos selecionados pelo usuário
+  if (letterInput.checked) {
+    funcGenerator.push(getLetterLowercase, getLetterUpercase);
+  }
+
+  if (numberInput.checked) {
+    funcGenerator.push(getNumber);
+  }
+
+  if (symbolInput.checked) {
+    funcGenerator.push(getSymbol);
+  }
+
+  // console.log(passwordLength, funcGenerator.length);
+  // se o usuário não marcou nada só retorno
+  if (passwordLength === 0 || funcGenerator.length === 0) {
+    return;
+  }
+
+
+
+  // limita o tamanho da password a 30 caracteres
+  if (passwordLength > 30) {
+    passwordLength = 30;
+    limite30.innerText= "Máximo de 30 caracteres"
+  }
+
+  // começa a gerar a senha alternando entre as funções que são 5 e limitadas a 10 caracteres
+  for (let i = 0; i < passwordLength; i = i + funcGenerator.length) {
+    // percorre o array, executando as funções de forma aleatória
+    // console.log("dentro do loop ${i}");
+    funcGenerator.forEach(() => {
+      const randomValue =
+        funcGenerator[Math.floor(Math.random() * funcGenerator.length)]();
+      password += randomValue;
+      // console.log(password);
+    });
+  }
+
+  // retira os dois carecteres adicionais pra ficar em 10
+  password = password.slice(0, passwordLength);
+  // console.log(password);
+
+  // tiro o display none da div pra mostrar a senha e coloca no h4 a senha gerada
+  passwordElement.style.display = "block";
+  passwordElement.querySelector("h4").innerText = password;
 };
-
-
 
 //
 //
@@ -102,12 +154,11 @@ getNumberAnotherWay) => {
 //
 
 // pressionar o botão de click here
+btnPassGen.addEventListener("click", () => {
+  genPassword(getLetterLowercase, getLetterUpercase, getSymbol, getNumber);
+});
+
+// ao pressionar o botão de selecionar a formação da senha abre o container de opções
 passwordBtn.addEventListener("click", () => {
-  genPassword(
-    getLetterLowercase,
-    getLetterUpercase,
-    getSymbol,
-    getNumber,
-    getNumberAnotherWay
-  );
+  showUp.classList.toggle("hide");
 });
